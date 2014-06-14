499aa8d96420059b4f937d7fb442046a36b30a36
do0fusz <do0fusz@hotmail.com>
2014-06-14 19:24:52 +0200

    overbodige afbeeldingen verwijdert

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,8 @@
 class Contact < MailForm::Base
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attributes :name, :subject, :message, validate: true
+  attribute  :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attributes :name, :subject, :message, validate: true 
   attributes :checker, captcha: true
+
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
